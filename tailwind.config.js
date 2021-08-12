7f6135bb9bd55cb6ea2f0a22d5cc766ef9d412dc
kosaki08 <claudioropez.k@gmail.com>
2021-08-12 23:04:42 +0900

    配色の設定を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGray: {
+          text: '#888',
+          minusOne: '#252525',
+          main: '#212121',
+        },
+      },
+    },
   },
   variants: {
     extend: {},

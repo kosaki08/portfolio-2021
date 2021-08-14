8d3b97d7821b2c60205277df3647b45dd9df6a11
kosaki08 <claudioropez.k@gmail.com>
2021-08-14 23:46:38 +0900

    Works一覧にコンテンツを追加

---
+++ src/components/works/WorksPage.style.ts
@@ -0,0 +1,20 @@
+import tw, { css } from 'twin.macro'
+
+const listItem = css`
+  ${tw`pb-16`}
+`
+
+const imageWrapper = css`
+  ${tw`pb-4`}
+  img {
+    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.2), 0 3px 30px rgba(0, 0, 0, 0.3),
+      0 30px 40px rgba(0, 0, 0, 0.5);
+  }
+`
+
+const description = css`
+  ${tw`text-xs text-darkGray-text`}
+  ${tw`md:(text-sm)`}
+`
+
+export { listItem, imageWrapper, description }
--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,31 +1,63 @@
 import Link from 'next/link'
 import { FC } from 'react'
-import tw, { css } from 'twin.macro'
 
 import SiteContainer from '../../containers/SiteContainer/SiteContainer.container'
 import DefaultPageLayout from '../../components/layouts/DefaultPage/DefaultPage.layout'
+import {
+  listItem,
+  imageWrapper,
+  description,
+} from '../../components/works/WorksPage.style'
+
+const data = [
+  {
+    title: '株式会社 UTエイム Webパンフレット',
+    href: '/works/ut-aim',
+    thumbPath: '/ut-aim/ut_aim_1.png',
+    tag: 'Web / Development',
+  },
+  {
+    title: '日本ワイン専門店 遅桜 イメージデザイン',
+    href: '/works/osozakura-image-design',
+    thumbPath: '/osozakura-image-design/osozakura_image_design_1.jpg',
+    tag: 'Web / Paper / Design',
+  },
+  {
+    title: '株式会社 犬の専門店 HappyLife',
+    href: '/works/happy-life',
+    thumbPath: '/happylife/happylife_1.png',
+    tag: 'Web / E-Commerce / Design / Development / WordPress',
+  },
+  {
+    title: '日本ワイン専門店 遅桜',
+    href: '/works/osozakura',
+    thumbPath: '/osozakura/osozakura_1.png',
+    tag: 'Web / Design / Development / WordPress',
+  },
+]
 
 const Works: FC = () => {
   return (
     <SiteContainer title="Works">
       <DefaultPageLayout pageKey="works">
-        <p css={[description]}>This is works page.</p>
-        <Link href="/works/osozakura" passHref>
-          <a>Osozakura</a>
-        </Link>
-        <Link href="/works/osozakura-ec" passHref>
-          <a>Osozakura EC</a>
-        </Link>
-        <Link href="/works/happy-life" passHref>
-          <a>HappyLife</a>
-        </Link>
+        <ul>
+          {data.map((item) => (
+            <li key={item.href} css={listItem}>
+              <Link href={item.href} passHref>
+                <a>
+                  <div css={imageWrapper}>
+                    <img src={item.thumbPath} alt={item.title} />
+                  </div>
+                  <h2 className="pb-1 text-sm md:text-base">{item.title}</h2>
+                  <p css={description}>{item.tag}</p>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </DefaultPageLayout>
     </SiteContainer>
   )
 }
-
-const description = css`
-  ${tw`text-2xl leading-6 text-center mt-6`}
-`
 
 export default Works

c8bc7971b033e460bf8d8c639b2177c46e377534
kosaki08 <claudioropez.k@gmail.com>
2021-08-15 13:16:38 +0900

    簡単なページ遷移アニメーションを実装

--- src/containers/SiteContainer/SiteContainer.container.tsx
+++ src/containers/SiteContainer/SiteContainer.container.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import NextNprogress from 'nextjs-progressbar'
+import { motion } from 'framer-motion'
 
 import Header from '../../components/global/Header/Header.component'
 import Footer from '../../components/global/footer'
@@ -12,6 +13,12 @@ import { siteContainer, contentWrapper } from './SiteContainer.style'
 type Props = {
   title: string
   children?: React.ReactNode
+}
+
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 80 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: 0 },
 }
 
 const DefaultPageContainer: FC<Props> = ({ title, children }) => {
@@ -28,7 +35,15 @@ const DefaultPageContainer: FC<Props> = ({ title, children }) => {
           <Head>
             <title>{title}</title>
           </Head>
-          <main>{children}</main>
+          <motion.main
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            transition={{ type: 'linear' }}
+          >
+            {children}
+          </motion.main>
         </div>
         <Footer />
         <SideNav />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,19 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import { GlobalStyles } from 'twin.macro'
-import { AnimateSharedLayout } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 
-import Loading from '../components/global/loading'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false)
-  const { events } = useRouter()
-
-  useEffect(() => {
-    const handleStart = () => setLoading(true)
-    const handleComplete = () => setLoading(false)
-
-    events.on('routeChangeStart', handleStart)
-    events.on('routeChangeComplete', handleComplete)
-    events.on('routeChangeError', handleComplete)
-
-    return () => {
-      events.off('routeChangeStart', handleStart)
-      events.off('routeChangeComplete', handleComplete)
-      events.off('routeChangeError', handleComplete)
-    }
-  }, [])
-
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <AnimateSharedLayout>
-        {loading && <Loading />}
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
         <Component {...pageProps} />
-      </AnimateSharedLayout>
+      </AnimatePresence>
       <GlobalStyles />
     </>
   )

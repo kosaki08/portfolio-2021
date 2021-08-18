b4b03bc686b3b055f0acba8a0db06aacc78c4783
kosaki08 <claudioropez.k@gmail.com>
2021-08-18 17:34:01 +0900

    トップページのタイトルをSVGで表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,16 @@
 import { FC, useEffect, useRef } from 'react'
 import tw, { css } from 'twin.macro'
-import Image from 'next/image'
 import { useAtom } from 'jotai'
 
 import SiteContainer from '../containers/SiteContainer/SiteContainer.container'
 import MainVisual from '../components/home/MainVisual.component'
+import HomeTitle from '../components/home/HomeTitle.component'
 import HomeCTAButton from '../components/home/Button.component'
-import TitleSVG from '../assets/images/title/txt_hello_world.svg'
 import { HomeStateAtom } from '../atoms/HomeStateAtom'
 import { pageWrapper } from '../styles/wrapperStyles'
-import settings from '../settings'
 
 const homeContents = css`
   ${tw`flex justify-center items-center flex-col container mx-auto px-4`}
-
-  & img {
-    ${tw`pb-20`}
-  }
-
-  @media (min-width: ${settings.sizes.md}px) {
-    max-width: calc(100% - 6rem * 2);
-  }
 `
 
 const Home: FC = () => {
@@ -67,9 +57,7 @@ const Home: FC = () => {
           <MainVisual />
         </div>
         <div css={homeContents}>
-          <div className="pb-20">
-            <Image src={TitleSVG} alt="Hello World!" draggable={false} />
-          </div>
+          <HomeTitle />
           <HomeCTAButton />
         </div>
       </div>

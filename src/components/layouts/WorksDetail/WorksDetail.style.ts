fac0200709d7865e9ca149d646ad54140373d795
kosaki08 <claudioropez.k@gmail.com>
2021-10-11 15:31:20 +0900

    works詳細ページの表示を更新

--- src/components/layouts/WorksDetail/WorksDetail.style.ts
+++ src/components/layouts/WorksDetail/WorksDetail.style.ts
@@ -1,19 +1,36 @@
 import tw, { css } from 'twin.macro'
 
+import settings from '../../../settings'
+
 const worksDetailHeader = css`
-  ${tw`relative max-w-7xl mx-auto `}
+  ${tw`relative max-w-7xl mx-auto`}
   ${tw`pb-20`}
 
   & > div {
     ${tw`md:(flex)`}
   }
 
-  & > span {
-    ${tw`absolute text-9xl font-montserrat font-black`}
+  h1 {
+    width: calc(100% - 3.1875rem - 1rem);
+  }
 
+  & > span {
+    ${tw`absolute text-4xl font-montserrat font-black`}
+
+    right: 1rem;
     bottom: 6rem;
-    right: 7rem;
     color: #4a4a4a;
+
+    @media (min-width: ${settings.sizes.md}px) {
+      ${tw`text-9xl `}
+      bottom: 3rem;
+      right: 1.5rem;
+    }
+
+    @media (min-width: ${settings.sizes.lg}px) {
+      bottom: 6rem;
+      right: 7rem;
+    }
   }
 
   .works-detail-mv,
@@ -22,10 +39,13 @@ const worksDetailHeader = css`
   }
 
   .works-detail-mv {
-    ${tw`md:(mb-10 pb-0)`}
+    ${tw`mb-6 mx-4`}
+    ${tw`md:(pb-0)`}
 
-    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.2), 0 3px 30px rgba(0, 0, 0, 0.3),
-      0 30px 40px rgba(0, 0, 0, 0.5);
+    & > div {
+      box-shadow: 0 2px 15px rgba(0, 0, 0, 0.2), 0 3px 30px rgba(0, 0, 0, 0.3),
+        0 30px 40px rgba(0, 0, 0, 0.5);
+    }
   }
 
   .works-detail-titles {

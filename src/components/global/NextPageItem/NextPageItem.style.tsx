0458ab394c2f9aa57a9dd53ae5eba90d015caf79
kosaki08 <claudioropez.k@gmail.com>
2021-10-11 17:20:50 +0900

    nextPageItemにtransitionを追加

--- src/components/global/NextPageItem/NextPageItem.style.tsx
+++ src/components/global/NextPageItem/NextPageItem.style.tsx
@@ -3,12 +3,24 @@ import settings from '../../../settings'
 
 const nextPageItem = css`
   a {
-    ${tw`relative block w-full`}
+    ${tw`relative block w-full overflow-hidden`}
     height: 16rem;
+
+    &:hover img {
+      ${tw`transform scale-125`}
+    }
+
+    &:hover strong {
+      letter-spacing: 0.5rem;
+    }
 
     @media screen and (min-width: ${settings.sizes.md}px) {
       height: 30rem;
     }
+  }
+
+  img {
+    transition: all 5s linear;
   }
 
   a::before {
@@ -16,7 +28,7 @@ const nextPageItem = css`
   }
 
   strong {
-    ${tw`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 text-2xl font-fjalla uppercase`}
+    ${tw`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 text-2xl font-fjalla uppercase duration-1000`}
     letter-spacing: .8rem;
   }
 `
